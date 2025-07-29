17ffd285c6090633b4c7eaefba5b92f8b802d2aa
yu320 <63506018+yu320@users.noreply.github.com>
2025-07-29 15:21:21 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // 設定要掃描的檔案路徑
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
